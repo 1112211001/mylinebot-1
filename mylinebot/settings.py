24efafccc44873d8a54996cd9d438ff6dd3968c7
1112211001 <1112211001@gap.wzu.edu.tw>
2025-01-07 17:19:13 +0800

    Update settings.py

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=q=)o^xsdx#2#2n%6t(lyay=#171=8@a!d(g+-(a3i8%31*-@q"
 
 # LINE Messaging API
-LINE_CHANNEL_SECRET = 'e7aede15e5e60ce4533b66233479e4d8'
-LINE_CHANNEL_ACCESS_TOKEN = 'KmkngR1xeSDVV5ZZmXHe2hi99tY0R69FUoeuVdtTyBQsHWOcWzVViyvLns9edonB8HsjpuaVj0gvIYCIimAJL+XQ6vOEALri8BFVySEueTjmhuICKceiuJbGReNwGz9A21BnI4XYb6C9w1tzN6HqzQdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_SECRET = '98d616fedcc9828160afa1767918c00b'
+LINE_CHANNEL_ACCESS_TOKEN = 'z0Rl5jdb97N4ThzQcmGMzgVPK6VuoogTKSXlSxE9rtYXsTFF6jmktmDstgb/9jTVoIMezgLFF4bNHEDBNWLWuuekYTgX5PeVa2LsmDuogcz78hDqRw5KgR0S/y5MAmNQKb49LGXfhdXYBJd3HpE60AdB04t89/1O/w1cDnyilFU='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
